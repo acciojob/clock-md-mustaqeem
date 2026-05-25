7ae1c38f96e6ddab0ba2754d2e59003ee6b897c9
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 22:44:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 const timer = document.getElementById("timer");
 
 function updateTimer() {
 
 	const now = new Date();
 
-	timer.innerText = now.toString();
+	timer.innerText = now.toLocaleString();
 }
 
 updateTimer();
