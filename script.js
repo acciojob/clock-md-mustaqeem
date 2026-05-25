356d0ade5ea85f2440f3ab92bca771e57d4b11fc
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 22:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+
+const timer = document.getElementById("timer");
+
+function updateTimer() {
+
+	const now = new Date();
+
+	timer.innerText = now.toString();
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
